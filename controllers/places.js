0fe4bea46774d14fc6cfc7cfe01fbd3a0b34cb78
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-23 10:33:19 -0700

    Adding 404 page view

--- controllers/places.js
+++ controllers/places.js
@@ -1,38 +1,7 @@
 const router = require('express').Router()
 
-// INDEX
 router.get('/', (req, res) => {
     res.send('GET /places')
-})
-
-// NEW
-router.get('/new', (req, res) => {
-    res.send('new stub')
-})
-
-// CREATE
-router.post('/', (req, res) => {
-    res.send('/places STUB')
-})
-
-// SHOW
-router.get('/arrayIndex', (req, res) => {
-    res.send('/places STUB')
-})
-
-// EDIT
-router.get('/arrayIndex/edit', (req, res) => {
-    res.send('/places STUB')
-})
-
-// UPDATE
-router.put('/arrayIndex', (req, res) => {
-    res.send('/places STUB')
-})
-
-// DELETE
-router.delete('/arrayIndex', (req, res) => {
-    res.send('/places STUB')
 })
 
 module.exports = router
