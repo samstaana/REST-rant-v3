df7beae551d051c86d109c81f1aabf5dc993a98e
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-25 00:11:52 -0700

    seeders added

--- controllers/places.js
+++ controllers/places.js
@@ -20,8 +20,18 @@ router.post('/', (req, res) => {
         res.redirect('/places')
     })
     .catch(err => {
-        console.log('err', err)
-        res.render('error404')
+        if (err && err.name == 'ValidationError') {
+            let message = 'Validation Error: '
+            for (var field in err.errors) {
+                message += `${field} was ${err.errors[field].value}. `
+                message += `${err.errors[field].message}`
+            }
+            console.log('Validation error message', message)
+            res.render('places/new', { message })
+        }
+        else {
+            res.render('error404')
+        }
     })
 })
 
@@ -44,17 +54,37 @@ router.get('/:id', (req, res) => {
 
 // UPDATE
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+    db.Place.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 // DELETE
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+    db.Place.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 // EDIT EXISTING PAGE
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+    db.Place.findById(req.params.id)
+        .then(place => {
+            res.redirect('places/edit', { place })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
 })
 
 // CREATE COMMENT
