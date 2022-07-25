4f07c36741ca6c4f82643e68fda5e78bcfa644ac
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-25 01:35:03 -0700

    Part 10 completed

--- controllers/places.js
+++ controllers/places.js
@@ -43,7 +43,9 @@ router.get('/new', (req, res) => {
 // SHOW
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+    .populate('comments')
     .then(place => {
+        console.log(place.comments)
         res.render('places/show', { place })
     })
     .catch(err => {
@@ -88,8 +90,25 @@ router.get('/:id/edit', (req, res) => {
 })
 
 // CREATE COMMENT
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+    .then(place => {
+        db.Comment.create(req.body)
+        .then(comment => {
+            place.comments.push(comment.id)
+            place.save()
+            .then(() => {
+                res.redirect(`/places/${req.params.id}`)
+            })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+    })
+    .catch(err => {
+        res.render('error404')
+    })
 })
 
 // DELETE COMMENT
