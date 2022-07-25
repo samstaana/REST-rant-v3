5c76c0e20cae8b0222dd4c031c39fddb1f38356b
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-24 22:16:13 -0700

    Delete functionality implemented

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 // Express Settings
 app.set('views', __dirname + '/views')
@@ -11,7 +12,8 @@ app.engine('jsx', require('express-react-views').createEngine())
 // Controllers & Routes
 app.use(express.static('public'))
 app.use('/places', require('./controllers/places'))
-app.use(express.urlencoded ({ extended: true }) )
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 
 app.get('/', (req, res) => {
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,12 +2,14 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, index) => {
       return (
         <div className="col-sm-6">
-          <a href={`/places/${index}`}>
-            <h2>{place.name}</h2>
-          </a>
+          <h2>
+            <a href={`/places/${index}`}>
+              {place.name}
+            </a>
+          </h2>
           <p className="text-center">
             {place.cuisines}
           </p>
